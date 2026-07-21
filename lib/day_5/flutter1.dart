42544015567aea06b621271bf22acf247fe27166
audy <audyliaaskawidiaputri@gmail.com>
2026-07-21 14:15:34 +0700

    Tambahkan Flutter1

---
+++ lib/day_5/flutter1.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Flutter1 extends StatelessWidget {
+  const Flutter1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xffBD1B1B),
+        title: const Text("Profil Saya"),
+        centerTitle: true,
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              "Audylia Aska Widiaputri",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Icon(Icons.location_on), Text("Jakarta")],
+            ),
+
+            Text(
+              "Seorang peserta pelatihan yang sedang mendalami Flutter di PPKD.",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
