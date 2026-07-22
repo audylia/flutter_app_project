903f26bf460d03a0a25da3cda0578156e69d4bb9
audy <audyliaaskawidiaputri@gmail.com>
2026-07-22 14:44:41 +0700

    Update Flutter2

---
+++ lib/day_6/flutter2.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Flutter2 extends StatelessWidget {
+  const Flutter2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
