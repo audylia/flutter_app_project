f2ccd483c595e7efc66665eb3b6c6a985dbf1d68
audy <audyliaaskawidiaputri@gmail.com>
2026-07-22 14:47:30 +0700

    Update Flutter2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_project/day_6/expanded.dart';
+import 'package:flutter_app_project/day_6/flutter2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: ExpandedDay6(),
+      home: Flutter2(),
     );
   }
 }
